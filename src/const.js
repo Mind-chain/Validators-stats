b039ae2d439bedd46b94d0b521a522546a5c0652
nnlgsakib <nlgarts@outlook.com>
2024-02-26 16:24:58 +0600

    removed useless code

--- src/const.js
+++ src/const.js
@@ -3,12 +3,10 @@ const contracts = {
     Pmind : "0x75E218790B76654A5EdA1D0797B46cBC709136b0"
 }
 const ws_rpc = "wss://seednode.mindchain.info/ws"
-const val_statusapi = "https://mainnet.mindscan.info/api/v2/addresses"
 const blockcounterapi = "https://mainnet.mindscan.info/api/v2/addresses/"
 
 module.exports = {
     contracts,
     ws_rpc,
-    blockcounterapi,
-    val_statusapi
+    blockcounterapi
 }
