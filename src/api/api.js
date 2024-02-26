85fadfab39dd2920b3fd92f214c7ac4e6b15944c
nnlgsakib <nlgarts@outlook.com>
2024-02-26 16:22:55 +0600

    init

---
+++ src/api/api.js
@@ -0,0 +1,55 @@
+const ethers = require('ethers');
+const axios = require('axios');
+const { validator_abi } = require('./../abi/validator.js');
+const { erc20_abi } = require('./../abi/erc20.js');
+const { contracts, ws_rpc, blockcounterapi } = require('./../const');
+
+const provider = new ethers.providers.WebSocketProvider(ws_rpc);
+const validatorContract = new ethers.Contract(contracts.validator, validator_abi, provider);
+const rewardTokenContract = new ethers.Contract(contracts.Pmind, erc20_abi, provider);
+const API_ENDPOINT = '/validators';
+
+async function fetchValidatorData(validator) {
+    const [stake, rewards, statusData, counterData] = await Promise.all([
+        validatorContract.accountStake(validator),
+        rewardTokenContract.balanceOf(validator),
+        axios.get(`${blockcounterapi}${validator}`),
+        axios.get(`${blockcounterapi}${validator}/counters`)
+    ]);
+
+    const validatedBlocksStatus = statusData.data.has_validated_blocks ? "active" : "inactive";
+    const validatedBlocksCount = counterData.data.validations_count || 0; // Default to 0 if no data is available
+    return {
+        address: validator,
+        stake: ethers.utils.formatEther(stake) + " MIND",
+        rewards: ethers.utils.formatEther(rewards) + " PMIND",
+        validatedBlocksCount: validatedBlocksCount,
+        validatedBlocksStatus: validatedBlocksStatus,
+    };
+}
+
+async function fetchValidators() {
+    const validators = await validatorContract.validators();
+    const promises = validators.map(fetchValidatorData);
+
+    return await Promise.all(promises);
+}
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get(API_ENDPOINT, async (req, res) => {
+    try {
+        console.log('Received HTTP request for validator data.'); // Added log statement
+        const validatorData = await fetchValidators();
+        res.json(validatorData);
+    } catch (error) {
+        console.error('Error fetching validator data:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ src/const.js
@@ -0,0 +1,14 @@
+const contracts = {
+    validator  : "0x0000000000000000000000000000000000001001",
+    Pmind : "0x75E218790B76654A5EdA1D0797B46cBC709136b0"
+}
+const ws_rpc = "wss://seednode.mindchain.info/ws"
+const val_statusapi = "https://mainnet.mindscan.info/api/v2/addresses"
+const blockcounterapi = "https://mainnet.mindscan.info/api/v2/addresses/"
+
+module.exports = {
+    contracts,
+    ws_rpc,
+    blockcounterapi,
+    val_statusapi
+}
