c0d80bbc2737a85e8af2a58bbdc6aa7a97c13101
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-03-12 01:38:39 +0600

    Update const.js

--- src/const.js
+++ src/const.js
@@ -3,8 +3,8 @@ const contracts = {
     Pmind : "0x75E218790B76654A5EdA1D0797B46cBC709136b0",
     blockchainInfoAddress: "0xa35fe650cC2A4F2024A73bA7f76bF7FBad64101F"
 }
-const ws_rpc = "wss://seednode.mindchain.info/ws"
-const http_rpc = "https://seednode.mindchain.info";
+const ws_rpc = "ws://91.208.92.6:8545/ws"
+const http_rpc = "http://91.208.92.6:8545";
 const blockcounterapi = "http://91.208.92.77:3000/api/v2/addresses/"
 
 module.exports = {
